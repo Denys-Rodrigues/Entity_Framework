8c03389160545d6b369a6f5a26b4a6e5d49a6970
DW Rodrigues <denys.w.r@hotmail.com>
2021-08-16 21:24:45 -0300

    Adicionar arquivos de projeto.

---
+++ Entity_Framework/Context/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using Entity_Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Entity_Framework.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("EF")
+        {
+            DropCreateDatabaseIfModelChanges<EFContext> initializer = new DropCreateDatabaseIfModelChanges<EFContext>();
+            Database.SetInitializer<EFContext>(initializer);
+        }
+
+        public DbSet<Editora> Editoras { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ Entity_Framework/Models/Livro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Entity_Framework.Models
+{
+    [Table("tbl_livros")]
+    public class Livro
+    {
+        [Key]
+        public long LivroID { get; set; }
+        
+        [Column("nome_do_livro")]
+        [Required]
+        [MaxLength(30)]
+        public string Titulo { get; set; }
+        
+        public double Preco { get; set; }
+
+        [NotMapped]
+        public string data { get; set; }
+    }
+}
